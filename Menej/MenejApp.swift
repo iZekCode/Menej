d07b1877a5f3860a0051d22fd1767f8ce57cfd62
Filbert Naldo Wijaya <142918945+iZekCode@users.noreply.github.com>
2026-07-19 15:52:18 +0700

    Initial Commit

---
+++ Menej/MenejApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenejApp.swift
+//  Menej
+//
+//  Created by Filbert Naldo Wijaya on 19/07/26.
+//
+
+import SwiftUI
+
+@main
+struct MenejApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
